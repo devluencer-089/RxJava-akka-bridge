dee98cc01b8e7ee9809de92a74c3e2ff0c72e1a4
Michael Omann <michael.omann@gmail.com>
2018-02-03 19:22:09 +0100

    implement Disposable contract on Worker

--- src/main/java/com/michaelomann/rxjava/akkabridge/scheduler/AkkaScheduler.java
+++ src/main/java/com/michaelomann/rxjava/akkabridge/scheduler/AkkaScheduler.java
@@ -4,6 +4,8 @@ import akka.actor.ActorSystem;
 import akka.actor.Cancellable;
 import akka.actor.Scheduler;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.disposables.EmptyDisposable;
+import io.reactivex.plugins.RxJavaPlugins;
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.duration.FiniteDuration;
 
@@ -26,10 +28,11 @@ public class AkkaScheduler extends io.reactivex.Scheduler {
         return new AkkaWorker(scheduler, dispatcher);
     }
 
-    static class AkkaWorker extends Worker {
+    static class AkkaWorker extends io.reactivex.Scheduler.Worker {
 
         private final Scheduler scheduler;
         private final ExecutionContext dispatcher;
+        private Disposable disposable = EmptyDisposable.INSTANCE;
 
         public AkkaWorker(Scheduler scheduler, ExecutionContext dispatcher) {
             this.scheduler = scheduler;
@@ -38,16 +41,23 @@ public class AkkaScheduler extends io.reactivex.Scheduler {
 
         public Disposable schedule(Runnable run, long delay, TimeUnit unit) {
             FiniteDuration duration = FiniteDuration.create(delay, unit);
-            Cancellable cancellable = scheduler.scheduleOnce(duration, run, dispatcher);
-            return RxDisposables.from(cancellable);
+            try {
+                Cancellable cancellable = scheduler.scheduleOnce(duration, run, dispatcher);
+                disposable = RxDisposables.from(cancellable);
+                return this;
+            } catch (Exception ex) {
+                RxJavaPlugins.onError(ex);
+                return EmptyDisposable.INSTANCE;
+            }
         }
 
         public void dispose() {
-
+            if (!disposable.isDisposed())
+                disposable.dispose();
         }
 
         public boolean isDisposed() {
-            return true;
+            return disposable.isDisposed();
         }
     }
 }
--- src/test/java/com/michaelomann/rxjava/akkabridge/scheduler/AkkaSchedulerTest.java
+++ src/test/java/com/michaelomann/rxjava/akkabridge/scheduler/AkkaSchedulerTest.java
@@ -4,6 +4,9 @@ import akka.actor.ActorSystem;
 import akka.actor.Terminated;
 import akka.dispatch.MessageDispatcher;
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.plugins.RxJavaPlugins;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +17,14 @@ import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static com.jayway.awaitility.Awaitility.await;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class AkkaSchedulerTest {
@@ -33,37 +43,78 @@ public class AkkaSchedulerTest {
     public void shutdown() throws Exception {
         initMocks(this);
         Future<Terminated> terminate = actorSystem.terminate();
-        Await.result(terminate, Duration.create(3, TimeUnit.SECONDS));
+        Await.result(terminate, Duration.create(3, SECONDS));
     }
 
     @Test
-    public void itSchedulesWork() {
+    public void itSchedulesWorkOnMainDispatcher() {
         AkkaScheduler scheduler = new AkkaScheduler(actorSystem);
 
-        Observable.rangeLong(1, 5)
+        TestObserver<String> observer = Observable.rangeLong(1, 5)
                 .doOnNext(value -> logger.info("received: {}", value))
+                .map(value -> Thread.currentThread().getName())
                 .subscribeOn(scheduler)
-                .blockingSubscribe();
+                .test();
+
+        observer.awaitTerminalEvent();
+        assertThat(observer.values())
+                .hasSize(5)
+                .allSatisfy(threadName -> assertThat(threadName).startsWith("default-akka.actor.default-dispatcher"));
     }
 
     @Test
-    public void itSchedulesWorkWithTimedDelay() {
+    public void itSchedulesWorkWithTimedDelayOnMainDispatcher() {
         AkkaScheduler scheduler = new AkkaScheduler(actorSystem);
 
-        Observable.interval(1, TimeUnit.SECONDS, scheduler)
+        AtomicInteger schduledWorkCounter = new AtomicInteger(0);
+        RxJavaPlugins.setScheduleHandler(runnable -> {
+            schduledWorkCounter.incrementAndGet();
+            return runnable;
+        });
+
+        TestObserver<String> observer = Observable.interval(10, MILLISECONDS, scheduler)
                 .take(3)
                 .doOnNext(value -> logger.info("received: {}", value))
+                .map(value -> Thread.currentThread().getName())
                 .subscribeOn(scheduler)
-                .blockingSubscribe();
+                .test();
+
+        observer.awaitTerminalEvent();
+        assertThat(schduledWorkCounter).hasValue(3);
+        assertThat(observer.values())
+                .hasSize(3)
+                .allSatisfy(threadName -> assertThat(threadName).startsWith("default-akka.actor.default-dispatcher"));
     }
+
     @Test
     public void itSchedulesWorkOnADedicatedDispatcher() {
         MessageDispatcher customDispatcher = actorSystem.dispatchers().lookup("my-fixed-size-dispatcher");
         AkkaScheduler scheduler = new AkkaScheduler(actorSystem.scheduler(), customDispatcher);
 
-        Observable.rangeLong(1, 5)
+        TestObserver<String> observer = Observable.rangeLong(1, 5)
                 .doOnNext(value -> logger.info("received: {}", value))
                 .subscribeOn(scheduler)
-                .blockingSubscribe();
+                .map(value -> Thread.currentThread().getName())
+                .subscribeOn(scheduler)
+                .test();
+
+        observer.awaitTerminalEvent();
+        observer.awaitTerminalEvent();
+        assertThat(observer.values())
+                .hasSize(5)
+                .allSatisfy(threadName -> assertThat(threadName).startsWith("default-my-fixed-size-dispatcher"));
+    }
+
+    @Test
+    public void itCancelsScheduledWorkWhenDisposed() {
+        AkkaScheduler sut = new AkkaScheduler(actorSystem);
+        Object spy = spy(new Object());
+
+        Disposable disposable = sut.scheduleDirect(() -> spy.hashCode(), 10, TimeUnit.MILLISECONDS);
+        disposable.dispose();
+
+        await().atMost(1, SECONDS).until(() -> {
+            verifyZeroInteractions(spy);
+        });
     }
 }
